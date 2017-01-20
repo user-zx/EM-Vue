74f02dc058ce8ae232393441a2bf3fa923882d34
yubing <29988076@qq.com>
2017-01-20 17:45:50 +0800

    优化初始项目结构

--- em/src/main.js
+++ em/src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from '../node_modules/vue/dist/vue.min.js' 
+import App from './App.vue' 
+import VueRouter from '../node_modules/vue-router/dist/vue-router.min.js'
+import routerConfig from "./router.config.js"
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
-         
+Vue.use(VueRouter);
+   
