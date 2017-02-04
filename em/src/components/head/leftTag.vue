b30241b6b9023536c6117f4206a634820566a640
zhangxin <zhangxin11032@vip.qq.com>
2017-02-04 13:38:29 +0800

    框架

--- em/src/components/head/leftTag.vue
+++ em/src/components/head/leftTag.vue
@@ -1,19 +1,35 @@
 <template>
     <div class="leftTag" id="leftTag">
-        <div class="nav-box">
-            <ul class="nav nav-pills nav-stacked">
-                <li><a href="javascript:void(0);"><i></i>销售线索</a></li>
-                <li><a href="javascript:void(0);"><i></i>我的收藏</a></li>
-                <li><a href="javascript:void(0);"><i></i>自动匹配</a></li>
-                <li><a href="javascript:void(0);"><i></i>查看记录</a></li>
-                <li><a href="javascript:void(0);"><i></i>回收站</a></li>
-            </ul>
-        </div>
+        <ul class="nav nav-pills nav-stacked">
+            <li class="active"><a href="javascript:void(0);"><i class="icons icons-1"></i>销售线索</a></li>
+            <li><a href="javascript:void(0);"><i class="icons icons-2"></i>我的收藏</a></li>
+            <li><a href="javascript:void(0);"><i class="icons icons-3"></i>自动匹配</a></li>
+            <li><a href="javascript:void(0);"><i class="icons icons-4"></i>查看记录</a></li>
+            <li><a href="javascript:void(0);"><i class="icons icons-5"></i>回收站</a></li>
+            <li class="last"><a href="javascript:void(0);"><i class="icons icons-6"></i>设置</a></li>
+        </ul>
     </div>
 </template>
 <style>
-    .leftTag{float: left;}
-    .leftTag .nav-box{width: 135px;}
+    .leftTag{position: fixed;left:0;top:65px;bottom: 0;padding-top:25px;width:135px;background-color: #273e4c;}
+    .leftTag>ul>li.last{position: fixed;left:0;bottom: 25px;width:135px;}
+    .leftTag>ul>li>a{text-align: center;color:#ffffff;border-left:2px solid transparent;}
+    .leftTag>ul>li>a>.icons{display: block;width:35px;height:35px;margin:0 auto;background-image: url("../../assets/images/left_top_icon.png");background-repeat: no-repeat;}
+    /*.icons-1{background-position: -1px 10px;}*/
+    .icons-1{background-position: 0 -34px;}
+    .icons-2{background-position: 0 -127px;}
+    .icons-3{background-position: 0 -226px;}
+    .icons-4{background-position: 0 -324px;}
+    .icons-5{background-position: 0 -421px;}
+    .icons-6{background-position: 0 -503px;}
+
+    .leftTag>ul>li>a:hover,.leftTag>ul>li>a:focus,.leftTag>ul>li.active>a,.nav-pills>li.active>a, .nav-pills>li.active>a:focus, .nav-pills>li.active>a:hover{border-left-color:#32ccca;border-radius:0;background-color: #0b1c26;}
+    .leftTag>ul>li>a:hover>.icons-1,.leftTag>ul>li>a:focus>.icons-1,.leftTag>ul>li.active>a>.icons-1{background-position: -42px -34px;}
+    .leftTag>ul>li>a:hover>.icons-2,.leftTag>ul>li>a:focus>.icons-2,.leftTag>ul>li.active>a>.icons-2{background-position: -42px -127px;}
+    .leftTag>ul>li>a:hover>.icons-3,.leftTag>ul>li>a:focus>.icons-3,.leftTag>ul>li.active>a>.icons-3{background-position: -42px -226px;}
+    .leftTag>ul>li>a:hover>.icons-4,.leftTag>ul>li>a:focus>.icons-4,.leftTag>ul>li.active>a>.icons-4{background-position: -42px -324px;}
+    .leftTag>ul>li>a:hover>.icons-5,.leftTag>ul>li>a:focus>.icons-5,.leftTag>ul>li.active>a>.icons-5{background-position: -42px -421px;}
+    .leftTag>ul>li>a:hover>.icons-6,.leftTag>ul>li>a:focus>.icons-6,.leftTag>ul>li.active>a>.icons-6{background-position: -42px -503px;}
 </style>
 <script>
     export default {
@@ -24,4 +40,9 @@
             };
         }
     }
+    $(function(){
+       $(".leftTag>ul>li>a").on("click",function(){
+           $(this).parent().addClass("active").siblings().removeClass("active");
+       });
+    });
 </script>
--- em/src/components/personal/Home.vue
+++ em/src/components/personal/Home.vue
@@ -1,8 +1,11 @@
 <template>
-	<div id="home">
+	<div class="clearfix">
 		<heads></heads>
         <div class="wrapper">
             <left-tag></left-tag>
+            <div class="wrapper-content">
+
+            </div>
         </div>
 	</div>
 </template>
@@ -10,10 +13,11 @@
 	import heads from '../head/heads.vue';
 	import leftTag from '../head/leftTag.vue';
     export default {
-        name:"home",
         components:{heads,leftTag}
 	}
 </script>
 <style>
-	
+    body{background-color: #f2f2f2;}
+    .wrapper{margin-left:135px;}
+    .wrapper .wrapper-content{width: 100%;padding:25px 20px;}
 </style>
