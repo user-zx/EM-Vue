bd330ea1fa881be626b2e9b560dc4e9e91c0a666
张鑫 <zhangxin11032@vip.qq.com>
2017-02-07 13:55:52 +0800

    下载bootstrap-select依赖

--- em/src/components/views/viewLog.vue
+++ em/src/components/views/viewLog.vue
@@ -1,8 +1,50 @@
 <template>
-<div>
-	<h3>asdasd</h3>
-</div>
-	
+	<div class="viewLog">
+		<div class="search-box clearfix">
+			<form class="form-horizontal">
+				<div class="col-md-2">
+					<select class="form-control">
+						<option>线索来源</option>
+						<option>线索来源</option>
+						<option>线索来源</option>
+						<option>线索来源</option>
+						<option>线索来源</option>
+						<option>线索来源</option>
+					</select>
+				</div>
+				<div class="col-md-2">
+					<select class="form-control">
+						<option>线索类型</option>
+						<option>线索类型</option>
+						<option>线索类型</option>
+						<option>线索类型</option>
+						<option>线索类型</option>
+						<option>线索类型</option>
+					</select>
+				</div>
+				<div class="col-md-2">
+					<select class="form-control">
+						<option>标记状态</option>
+						<option>标记状态</option>
+						<option>标记状态</option>
+						<option>标记状态</option>
+						<option>标记状态</option>
+						<option>标记状态</option>
+					</select>
+				</div>
+				<div class="col-md-2">
+					<select class="form-control">
+						<option>发布时间</option>
+						<option>发布时间</option>
+						<option>发布时间</option>
+						<option>发布时间</option>
+						<option>发布时间</option>
+						<option>发布时间</option>
+					</select>
+				</div>
+			</form>
+		</div>
+	</div>
 </template>
 <script>
 	export default {
