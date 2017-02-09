84459fa942e81bd0bfd70631f722f514ff09f85e
yubing <29988076@qq.com>
2017-02-09 14:48:45 +0800

    修改分页css

--- em/src/components/views/sellClue.vue
+++ em/src/components/views/sellClue.vue
@@ -46,17 +46,18 @@
 	</div>
 	
 	<div class="sellClue_list">
-		<div class="sellClue_list_div" v-for="item in dataList">
+		<div class="sellClue_list_div" v-for="(item,index) in dataList">
+			{{index}} 
 			<span>{{item.type}}</span>
-			<h4>{{item.title}}</h4>
-		    <div class="sellClue_list_div_div"> <span><i>关键词:</i>{{item.keywords}}</span> <span><i>发布者:</i>{{item.author}}</span><span><i>发布时间:</i>2016-1-1 11:11:11</span><span><i>线索来源:</i>百度贴吧</span></div>
-		    <p>我和王医生见面的地方在一个商场里的咖啡休息区。这是一位在行业里口碑和声望都很不错、拥有十几年经验的医生，属于手术基本要排到3个月之后的那种。不过和其他医生不同，王医生每天接触的“病人”是一群想要变美的女性。当美图秀秀等P图软件不再能满足中国姑娘们变美需求的时候，“整形”、“美容”、“动刀”早就不是一件遥远而可怕的事情。像王医生这样小有名气的整形美容医生，也越来越多的受到消费者的追捧</p>
+			<h4>{{item.title}}</h4> 
+		    <div class="sellClue_list_div_div"> <span><i>关键词:</i>{{item.keywords}}</span> <span><i>发布者:</i>{{item.author}}</span><span><i>发布时间:</i>{{item.time}}</span><span><i>线索来源:</i>{{item.source}}</span></div>
+		    <p>{{item.content}}</p>
 		    <ul class="sellClue_list_div_ul">
-		        <li><i class="glyphicon glyphicon-heart-empty"></i>收藏线索</li>
-		        <li><img src="../../assets/images/forgetClue.png" height="16" width="16">忽略线索</li>
-		        <li><i class="glyphicon glyphicon-flag"></i>标记处理</li>
+		        <li><a href="javascript:;" class="btn"><i class="glyphicon glyphicon-heart-empty"></i>收藏线索</a></li>
+		        <li><a href="javascript:;" class="btn"><img src="../../assets/images/forgetClue.png" height="16" width="16">忽略线索</a></li>
+		        <li><a href="javascript:;" class="btn"><i class="glyphicon glyphicon-flag"></i>标记处理</a></li>
 		    </ul>
-
+			
 		    <menu class="clearfix">
 	    	    <li><img src="../../assets/images/location.png" height="25" width="22" alt=""><strong>北京市海淀区丹棱街3号</strong></li>
 	    	    <li><img src="../../assets/images/phone.png" height="22" width="18"><strong>13284191177</strong></li>
@@ -88,7 +89,16 @@
 		    	<button>联系人信息</button>
 		    </menu> 
 		</div>
+		<div class="pageList clearfix" >
+			<ul class="clearfix">
+			    <li v-for="item in pageList">{{item}}</li>
+		     </ul>
+		     <span>
+		     	
+		     </span>
+		</div>
 		
+
 	</div>
 	
 </div>
@@ -106,6 +116,7 @@
 				timeList:["发布时间","不限","今天","昨天","自定义时间"],
 				bodyDataUrl:"apis/salesLeads/getHomePageSaleLeadsList",
 				dataList:[],
+				pageList:["1","2","3","4","5","6","7","8","9"]
 			}
 		},
 		methods:{
@@ -117,18 +128,12 @@
 				"pageNumber": 1,
 				 "pageSize": 10
 			}
-
 			vm.$http.post(vm.bodyDataUrl,pagesize).then((response)=>{
-				/*console.log(response);
-				//const */
 				const list = response.data.data.list;
-				console.log(list);
-				//vm.dataList = list;
-				
 				for (var prop in list) { 
 					const time = new Date(list[prop].publishDate).Format("yyyy-MM-dd hh:mm:ss");
-					vm.dataList[prop] =  {'type':list.type,'title':list.title,'keywords':list.keywords,'author':list.author,'time':time,'source':list.source,'content':list.content}
-					
+					const item = {'type':list[prop].type,'title':list[prop].title,'keywords':list[prop].keywords,'author':list[prop].author,'time':time,'source':list[prop].source,'content':list[prop].content};
+					vm.dataList.push(item);	
 				}
 			},(response)=>{
 				
@@ -138,4 +143,5 @@
 </script>
 <style scoped>
 	@import url("../../assets/style/page.css");
+	@import url("../../assets/style/pageList.css");
 </style>
