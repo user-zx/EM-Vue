45d1e35340be410554edc92581cefa5759098052
张鑫 <zhangxin11032@vip.qq.com>
2017-02-10 18:39:24 +0800

    设置页面

--- em/src/components/views/set.vue
+++ em/src/components/views/set.vue
@@ -72,7 +72,7 @@
 					<div class="panel-heading">
 						<h4 class="panel-title">
 							套餐信息
-							<a href="javascript:void(0);" class="btn btn-search">立即充值</a>
+							<a href="javascript:void(0);" class="btn btn-search-o">立即充值</a>
 						</h4>
 					</div>
 					<div class="panel-body">
@@ -110,10 +110,41 @@
 				</div>
 			</div>
 			<div id="combo" class="tab-pane fade">
-				套餐充值
+				<div class="panel panel-em">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-3">
+								<div class="combo-box">
+									<div class="combo-body">
+										<h5 class="comboName">套餐名称 <span class="price">¥199</span></h5>
+										<p>内含<span class="text-em">100次</span>线索查看</p>
+									</div>
+									<a href="javascript:void(0);" class="btn btn-combo">立即充值</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div id="keyWordSet" class="tab-pane fade">
-				关键词设置
+				<div class="panel panel-em">
+					<div class="panel-body">
+						<div class="search-box clearfix">
+							<button class="btn btn-search-o" type="button">添加关键词</button>
+							<div class="navbar-form navbar-right" role="search">
+								<div class="input-group">
+									<input type="text" class="form-control input-search" placeholder="输入关键词进行查询">
+									<span class="input-group-btn">
+										<button class="btn btn-search" type="button">搜索</button>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="table-reponse">
+							
+						</div>
+					</div>
+				</div>
 			</div>
 			<div id="expenseCalendar" class="tab-pane fade">
 				消费记录
@@ -129,6 +160,20 @@
 	.nav-tabs>li>a:hover{background-color: #ffffff;border-bottom-color:rgba(50,204,202,.8); }
 	.panel.panel-em>.panel-heading{border-bottom:1px solid #ededed;background-color: #ffffff;}
 	.panel.panel-em{padding:20px;}
+	.panel-em .panel-heading .panel-title{position: relative;color: #333333;font-size: 16px;border-left:2px solid #32ccca;padding:5px 5px;}
+	.panel-em .panel-heading .panel-title .warning-tips{margin-left:15px;color:#999999;font-size:14px;}
+	.panel-em .panel-heading .panel-title .warning-tips .warning-tel{color:#32ccca;font-size:18px;}
+	.panel-em .panel-heading .panel-title .btn-search-o{position: absolute;right: 0;bottom:0;color:#32ccca;}
+	.panel-em .panel-heading .panel-title .btn-search-o:hover,
+	.panel-em .panel-heading .panel-title .btn-search-o:focus{color:#FFFFFF;}
+	.panel-em .panel-body .control-label{color:#999999;font-weight: 400;}
+	.panel-em .panel-body .form-control-static .user-type{color:#ff9b50;}
+	.panel-em .panel-body .form-control-static .update-password{color:#32ccca;margin-left: 5px;}
+	.panel-em .panel-body .text-em{color: #32ccca;margin: 0 3px;}
+	.combo-box{background-color: #f2f2f2;border-radius:5px 5px 0 0;}
+	.combo-box .combo-body{padding:15px 20px;}
+	.combo-box .comboName{font-size:18px;color:#333333;}
+	.combo-box .comboName .price{float: right;color:#32ccca;}
 </style>
 <script>
 	export default {
