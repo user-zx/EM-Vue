de0889e0650cfbf6c1cb2db6b75beaa53c9ec152
yubing <29988076@qq.com>
2017-03-08 10:02:03 +0800

    添加文件

---
+++ em/src/components/prompt/expense.vue
@@ -0,0 +1,104 @@
+<template>
+	<div>
+		<div id="expense" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="expenseTitle" aria-hidden="true">
+			 <div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+							  <h4 class="text-center" v-if="status">点击查看将会扣除一次查看机会,是否继续</h4>
+							  <h4 class="text-center" v-else >余额不足,请充值</h4>
+						</div> 
+						  <div class="modal-footer" v-if="status">  
+				                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button> 
+				                <button type="button" class="btn btn-primary" @click="continueToAdd">继续</button>
+		                  </div>
+		                  <div class="modal-footer" v-else>  
+				                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button> 
+		                  </div>
+					</div>		 	
+			  </div>
+		</div>
+	
+	</div>
+	
+
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				status:this.$store.state.expenseModel.status,
+				text:"",
+			}
+		},
+		methods:{
+			continueToAdd(){
+				
+				let vm = this;  
+				//console.log(vm.indexData.itemData.address);   
+				
+				vm.$http.post(vm.indexData.url,vm.indexData.index).then((result)=>{
+						
+				   if(result.ok){ 
+				   	 if(result.data.success){  
+				   	 	vm.indexData.itemData.address = result.data.data.address;
+				   	 	vm.indexData.itemData.phone = result.data.data.phone;
+				   	 	vm.indexData.itemData.email = result.data.data.email;
+				   	 	vm.indexData.itemData.ip = result.data.data.ip;
+				   	 	vm.indexData.itemData.wechat = result.data.data.wechat;
+				   	 	vm.indexData.itemData.qq = result.data.data.qq;  
+				   	 	$("#expense").modal("hide"); 
+				   	 	
+				   	 }else{ 
+				   	 	console.log('test'); 
+				   	 	vm.indexData.itemData.address = "";
+				   	 	vm.indexData.itemData.phone = "";
+				   	 	vm.indexData.itemData.email = "";
+				   	 	vm.indexData.itemData.ip = "";
+				   	 	vm.indexData.itemData.wechat = ""
+				   	 	vm.indexData.itemData.qq = "";
+				   	 	vm.text = result.data.message;
+				   	 	$("#expense").modal("hide");
+				   	 } 
+				   }		     
+					
+				},(err)=>{
+					alert(err);
+					return false;
+
+				});
+			}
+		}, 
+		mounted(){   
+			let vm =this;  
+			//console.log(vm.status);  
+
+			$("#expense").on("show.bs.modal",function() {
+				//console.log(vm.$store.state.expenseModel.status);			
+			}).on("shown.bs.modal",function(){ 
+				vm.status=vm.$store.state.expenseModel.status;
+			}).on("hide.bs.modal",function(){
+
+			}).on("hidden.bs.modal",function(){
+
+			});   
+
+		}, 
+		beforeMount(){  
+			//console.log(this.$store.state.expenseModel.status);
+			/*  vm.status=vm.$store.state.expenseModel.status; 
+			  console.log(vm.status);*/ 
+		},
+		props:["indexData"],
+	} 
+</script>
+
+<style scoped>
+	.modal{
+		z-index: 9999;
+	} 
+	.modal-footer{
+		text-align: center;
+	}
+</style>
---
+++ em/src/components/prompt/notSufficient.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="expense" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="expenseTitle" aria-hidden="true">
+		 <div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					  <h4 class="text-center">您的套餐不足,<router-link to="/set">请充值</router-link></h4>
+				</div>  
+				 
+				  <div class="modal-footer">   
+		                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button> 
+		                <button type="button" class="btn btn-primary" @click="continueToAdd">继续</button>
+                  </div>
+			</div>		 	
+		 </div>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+
+			}
+		},
+		methods:{
+			continueToAdd(){
+				console.log(this.indexData);
+				let vm = this;
+				
+				
+			}
+		}, 
+	} 
+</script>
+
+<style scoped>
+	.modal{
+		z-index: 9999;
+	} 
+	.modal-footer{
+		text-align: center;
+	}
+</style>
