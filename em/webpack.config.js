58bb6f160ee1104cbcf35839356c07ab821dd0d9
yubing <29988076@qq.com>
2017-03-15 13:48:14 +0800

    临时

--- em/webpack.config.js
+++ em/webpack.config.js
@@ -20,20 +20,9 @@ module.exports = {
                             'scss': 'vue-style-loader!css-loader!sass-loader',
                             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
                         }
-                        // other vue-loader options go here
                     }
                 },
-                /*{
-                  test: /\.js$/,
-                  loader: 'babel-loader',
-                  exclude: /node_modules/
-                },
-                {
-                  test: /\.(png|jpg|gif|svg)$/,
-                  loader: 'file-loader',
-                  options: {
-                    name: '[name].[ext]?[hash]'
-                  }*/
+               
                 {
                     test: /\.js$/,
                     loader: 'babel-loader',
@@ -59,14 +48,14 @@ module.exports = {
             'vue$': 'vue/dist/vue.common.js',
             'jquery': 'jquery'
         }
-    },
+    },  
     devServer: {
         historyApiFallback: true,
         noInfo: true
     },
     performance: {
         hints: false
-    },
+    }, 
     devtool: '#eval-source-map',
     plugins: [
         new webpack.ProvidePlugin({
@@ -75,8 +64,8 @@ module.exports = {
             "window.jQuery": "jquery"
         })
     ]
-}
-
+} 
+  
 if (process.env.NODE_ENV === 'production') {
     module.exports.devtool = '#source-map'
         // http://vue-loader.vuejs.org/en/workflow/production.html
